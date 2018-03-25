719f801767de2f8d99fa164130f0eebac48655eb
Sergio Visinoni <piffio@piffio.org>
2018-03-25 23:37:38 +0200

    Fix comment to make codeclimate happy

--- actions/profile.go
+++ actions/profile.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gobuffalo/buffalo"
 )
 
-// ProfileShowProfile default implementation.
+// ProfileShow show the user profile page
 func ProfileShow(c buffalo.Context) error {
 	return c.Render(200, r.HTML("profile/show.html"))
 }
