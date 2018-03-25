ca088795f0162a2c5863da7a20ecf3480af1c548
Sergio Visinoni <piffio@piffio.org>
2018-03-25 01:19:31 +0100

    Add initial test for User model

--- models/user_test.go
+++ models/user_test.go
@@ -1,7 +1,26 @@
-package models
+package models_test
 
-import "testing"
+import (
+	"github.com/gobuffalo/pop/nulls"
+	"github.com/piffio/myvol/models"
+)
 
-func Test_User(t *testing.T) {
-	t.Fatal("This test needs to be implemented!")
+func (as *ModelSuite) Test_User_Create() {
+	count, err := as.DB.Count("users")
+	as.NoError(err)
+	as.Equal(0, count)
+
+	u := &models.User{
+		Email:      nulls.NewString("sergio@example.com"),
+		Name:       "Sergio Visinoni",
+		Provider:   "gplus",
+		ProviderID: "123456abc",
+	}
+
+	err = as.DB.Save(u)
+	as.NoError(err)
+
+	count, err = as.DB.Count("users")
+	as.NoError(err)
+	as.Equal(1, count)
 }
